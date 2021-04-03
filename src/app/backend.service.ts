5084f7c07263673a2c42bfa2882b6abd11c817fb
Kan <katookas.1990@gmail.com>
2021-04-03 23:38:30 +0700

    Lab#3

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -1,14 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable, QueryList, ViewChildren } from '@angular/core';
 import { Product } from './models/product'
+import { ProductItemComponent } from './catalog/productitem/productitem.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
-
+  products: Product[];
   constructor() { }
 
   getProducts(): Product[] {
+    this.products = [];
+    this.products.push({
+    name: 'Orange',
+    price: 990
+    });
+    this.products.push({
+    name: 'Watermelon',
+    price: 1000
+    });
+    this.products.push({
+    name: 'Coconut',
+    price: 500
+    });
     return [];
     }
 }
